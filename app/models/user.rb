1076be7ea40acfc680e480d6d555487728bdbfd2
Alina Karzhavina <malina@MacBook-Air-Alina.local>
2021-04-26 23:18:50 +0300

    users_controller fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+    EMAIL_FORMAT = /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/
+    
+    has_many :events
+
+    validates :name, presence: true, length: {maximum: 35}
+    validates :email, presence: true, length: {maximum: 255}
+    validates :email, uniqueness: true 
+    validates :email, format: { with: EMAIL_FORMAT }
 end
