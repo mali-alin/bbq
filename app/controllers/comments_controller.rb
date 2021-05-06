f2901fc8cbb0f8153590c7c054c9d1aaacc7baac
Alina Karzhavina <malina@MacBook-Air-Alina.local>
2021-05-06 22:30:35 +0300

    comments & subscriptions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,7 @@ class CommentsController < ApplicationController
   # POST /comments
   def create
     @new_comment = @event.comments.build(comment_params)
-    @new_comment = current_user
+    @new_comment.user = current_user
 
     if @new_comment.save
       redirect_to @event, notice: I18n.t('controllers.comments.created')
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,14 +6,21 @@ class User < ApplicationRecord
     
   has_many :events
   has_many :comments
+  has_many :subscriptions
 
   validates :username, presence: true, length: {maximum: 35}
 
   before_validation :set_name, on: :create
+
+  after_commit :lnk_subscriptions, on: :create
   
   private
 
   def set_name
     self.username = "Товарищ №#{rand(777)}" if self.username.blank?
+  end
+
+  def link_subscriptions
+    Subscription.where(user_id: nil, user_email: user.email).update_all(user_id: user.id)
   end
 end
