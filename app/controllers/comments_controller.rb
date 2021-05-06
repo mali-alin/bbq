cc094defd5631b2aca74822b55192a5aa75de7c4
Alina Karzhavina <malina@MacBook-Air-Alina.local>
2021-05-06 12:51:01 +0300

    missing bracket added

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -21,7 +21,7 @@ class CommentsController < ApplicationController
     if current_user_can_edit?(@comment)
       @comment.destroy!
     else
-      message = {alert: I18n.t('controllers.comments.error'}
+      message = {alert: I18n.t('controllers.comments.error')}
     end
 
     redirect_to @event, message
