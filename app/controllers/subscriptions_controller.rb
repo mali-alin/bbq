cd7df21a56dd22689ffd9ea3964b813e31ad06b0
Alina Karzhavina <malina@MacBook-Air-Alina.local>
2021-05-09 16:52:27 +0300

    validates (subscription model) added

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -4,25 +4,15 @@ class SubscriptionsController < ApplicationController
 
   # POST /subscriptions
   def create
-    unless @event.user == current_user
-    # Болванка для новой подписки
-        @new_subscription = @event.subscriptions.build(subscription_params)
-        
-        unless User.find_by(email: @new_subscription.user_email).present?
-          @new_subscription.user = current_user
-        
-          if @new_subscription.save
-            # Если сохранилась успешно, редирект на страницу самого события
-            redirect_to @event, notice: I18n.t("controllers.subscriptions.created")
-          else
-            # если ошибки — рендерим здесь же шаблон события
-            render "events/show", alert: I18n.t("controllers.subscriptions.error")
-          end
-        else 
-          redirect_to @event, notice: I18n.t("controllers.subscriptions.existing_email")
-        end
-    else 
-      redirect_to @event
+    @new_subscription = @event.subscriptions.build(subscription_params)
+    @new_subscription.user = current_user
+      
+    if @new_subscription.save
+      # Если сохранилась успешно, редирект на страницу самого события
+      redirect_to @event, notice: I18n.t("controllers.subscriptions.created")
+    else
+      # если ошибки — рендерим здесь же шаблон события
+      render "events/show", alert: I18n.t("controllers.subscriptions.error")
     end
   end 
 
--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -12,7 +12,11 @@ class Subscription < ApplicationRecord
   validates :user, uniqueness: {scope: :event_id}, if: -> { user.present? }
 
   # для данного event_id один email может использоваться только один раз (если нет юзера, анонимная подписка)
-  validates :user_email, uniqueness: {scope: :event_id}, unless: -> { user.present? }
+  validates :user_email, uniqueness: {scope: :event_id, message: "Данный email зарегистрирован"}, unless: -> { user.present? }
+
+  validate :check_event_owner, if: -> { user.present? }
+
+  validate :check_email_registered, unless: -> { user.present? }
 
   # переопределяем метод, если есть юзер, выдаем его имя,
   # если нет -- дергаем исходный переопределенный метод
@@ -31,6 +35,18 @@ class Subscription < ApplicationRecord
       user.email
     else
       super
+    end
+  end
+
+  def check_event_owner
+     if event.user == user
+      errors.add(:base, message: I18n.t("controllers.subscriptions.subscribe_error"))
+     end
+  end
+
+  def check_email_registered
+    if User.find_by(email: user_email).present?
+      errors
     end
   end
 end
