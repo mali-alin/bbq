537e32fe305fcbdc4983ca5bebc1433aa069d0b2
Alina Karzhavina <malina@MacBook-Air-Alina.local>
2021-05-11 17:55:59 +0300

    foo
    Signed-off-by: Alina Karzhavina <malina@MacBook-Air-Alina.local>

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,8 +7,8 @@ if Rails.env.production?
       provider:              'AWS',
       aws_access_key_id:     ENV['S3_ACCESS_KEY'],
       aws_secret_access_key: ENV['S3_SECRET_KEY'],
-      host:                   => "s3-eu-west-1.amazonaws.com",
-      region:                 => "eu-west-1"
+      host:                  "s3-eu-west-1.amazonaws.com",
+      region:                "eu-west-1"
     }
 
     config.fog_directory  = ENV['S3_BUCKET_NAME']
