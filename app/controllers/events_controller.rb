5bb6a5c6d5c7f85134afefe30916d2644a6f34fd
Alina Karzhavina <malina@MacBook-Air-Alina.local>
2021-05-14 17:09:05 +0300

    feature 'pincode for event' added

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,7 +3,8 @@ class EventsController < ApplicationController
 
   before_action :set_event, only: [:show]
   before_action :set_current_user_event, only: [:edit, :update, :destroy] 
-  # before action :set_current_user_event, only: [:edit, :update, :destroy]
+
+  before_action :password_guard!, only: [:show]
 
   # GET /events
   def index
@@ -53,6 +54,30 @@ class EventsController < ApplicationController
   end
 
   private
+
+  def password_guard!
+    if @event.pincode.blank?
+      return true 
+    end
+
+    if signed_in? && current_user == @event.user
+      return true 
+    end
+
+    if params[:pincode].present? && @event.pincode_valid?(params[:pincode])
+      cookies.permanent["events_#{@event.id}_pincode"] = params[:pincode]
+    end
+
+    pincode = cookies.permanent["events_#{@event.id}_pincode"]
+    
+    unless @event.pincode_valid?(pincode)
+      if params[:pincode].present?
+        flash.now[:alert] = I18n.t('controllers.events.wrong_pincode')
+      end
+      render 'password_form'
+    end
+  end
+
   def set_current_user_event
     @event = current_user.events.find(params[:id])
   end
@@ -63,6 +88,6 @@ class EventsController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
   def event_params
-    params.require(:event).permit(:title, :address, :datetime, :description)
+    params.require(:event).permit(:title, :address, :datetime, :description, :pincode)
   end
 end
