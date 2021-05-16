a399a04a5a0301fe3d9b998c40c56714a2847037
Alina Karzhavina <malina@MacBook-Air-Alina.local>
2021-05-17 00:05:07 +0300

    fixed

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -2,32 +2,35 @@ ymaps.ready(init);
 var myMap;
 
 function init(){
-  address = document.getElementById('map').getAttribute('data-address');
-
-  myMap = new ymaps.Map("map", {
-      center: [55.76, 37.64],
-      zoom: 10
-  });
-
+  if (document.getElementById('map') != null) {
+    address = document.getElementById('map').getAttribute('data-address')
   
-  myGeocoder = ymaps.geocode(address);
 
-  myGeocoder.then(
-    function (res) {
-      coordinates = res.geoObjects.get(0).geometry.getCoordinates();
+    myMap = new ymaps.Map("map", {
+        center: [55.76, 37.64],
+        zoom: 10
+    });
 
-      myMap.geoObjects.add(
-          new ymaps.Placemark(
-            coordinates,
-            {iconContent: address},
-            {preset: 'islands#blueStretchyIcon'}
-          )
-      );
+    
+    myGeocoder = ymaps.geocode(address);
 
-      myMap.setCenter(coordinates);
-      myMap.setZoom(15);
-    }, function (err) {
-      alert('Ошибка при определении местаположения');
-    }
-  );
+    myGeocoder.then(
+      function (res) {
+        coordinates = res.geoObjects.get(0).geometry.getCoordinates();
+
+        myMap.geoObjects.add(
+            new ymaps.Placemark(
+              coordinates,
+              {iconContent: address},
+              {preset: 'islands#blueStretchyIcon'}
+            )
+        );
+
+        myMap.setCenter(coordinates);
+        myMap.setZoom(15);
+      }, function (err) {
+        alert('Ошибка при определении местаположения');
+      }
+    );
+  }
 }
