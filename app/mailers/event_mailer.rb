ead69d5a5a9c634d2295e4afd8e1cf702ee2950e
Alina Karzhavina <malina@MacBook-Air-Alina.local>
2021-05-24 15:31:58 +0300

    helper default_i18n_subject to event_mailer added

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -4,20 +4,21 @@ class EventMailer < ApplicationMailer
     @name = subscription.username
     @event = event
 
-    mail to: event.user.email, subject: "#{I18n.t("mailers.event_mailer.new_subscription")} #{event.title}"
+    mail to: event.user.email, subject: default_i18n_subject(event: event.title)
   end
 
   def comment(event, comment, email)
     @comment = comment
     @event = event 
 
-    mail to: email, subject: "#{I18n.t("mailers.event_mailer.new_subscription")} #{event.title}"
+    mail to: event.user.email, subject: default_i18n_subject(event: event.title)
+
   end
 
   def pic(event, photo, email)
     @photo = photo
     @event = event
 
-    mail to: email, subject: "#{I18n.t("mailers.event_mailer.new_subscription")} #{@event.title}"
+    mail to: event.user.email, subject: default_i18n_subject(event: event.title)
   end
 end
