a9d75f0e2958e9235286eb62c01a6e6379bf9159
Alina Karzhavina <malina@MacBook-Air-Alina.local>
2021-06-23 19:38:21 +0300

    associations modified

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
     
-  has_many :events
-  has_many :comments
-  has_many :subscriptions
+  has_many :events, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :subscriptions, dependent: :destroy 
 
   validates :username, presence: true, length: {maximum: 35}
 
